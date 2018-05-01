bba098314563317f4eaf1eb2d92fa79af3ca01de
jameiww <jamieww@outlook.com>
2018-05-01 09:01:41 +0800

    all local project

---
+++ Go/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"time"
+)
+
+func main() {
+	fmt.Println(math.Pi)
+	fmt.Printf("The time is ", time.Now())
+}
---
+++ Spring Boot/highlight_springmvc4/src/main/java/com/wisely/highlight_springmvc4/MyMvcConfig.java
@@ -0,0 +1,37 @@
+package com.wisely.highlight_springmvc4;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@EnableScheduling
+@ComponentScan("com.wisely.highlight_springmvc4")
+public class MyMvcConfig extends WebMvcConfigurerAdapter{
+    @Bean
+    public InternalResourceViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new
+                InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/classes/views/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setViewClass(JstlView.class);
+        return viewResolver;
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/index").setViewName("/index");
+        registry.addViewController("/sse").setViewName("/sse");
+        registry.addViewController("/async").setViewName("/async");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/assets/");
+    }
+}
