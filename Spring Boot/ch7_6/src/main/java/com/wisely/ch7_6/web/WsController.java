a928a07477ce4561fad23519d24f47adedd88078
jameiww <jamieww@outlook.com>
2018-05-02 23:08:57 +0800

    Added chat function and spring security

--- Spring Boot/ch7_6/src/main/java/com/wisely/ch7_6/web/WsController.java
+++ Spring Boot/ch7_6/src/main/java/com/wisely/ch7_6/web/WsController.java
@@ -2,9 +2,13 @@ package com.wisely.ch7_6.web;
 
 import com.wisely.ch7_6.domain.WiselyMessage;
 import com.wisely.ch7_6.domain.WiselyResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
 
 @Controller
 public class WsController {
@@ -13,5 +17,21 @@ public class WsController {
     public WiselyResponse say(WiselyMessage message) throws Exception {
         Thread.sleep(3000);
         return new WiselyResponse("Welcome, " + message.getName() + "!");
+    }
+
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/chat")
+    public void handleChat(Principal principal, String msg) {
+        if(principal.getName().equals("jamieww")) {
+            messagingTemplate.convertAndSendToUser("wisely",
+                    "/queue/notifications", principal.getName() + "-send:"
+                            + msg);
+        } else {
+            messagingTemplate.convertAndSendToUser("jamieww",
+                    "/queue/notifications", principal.getName() + "-send:"
+                            + msg);
+        }
     }
 }
