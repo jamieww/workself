cbbb2359aa3a50743f01df2ec1c0c203e89d5c37
jameiww <jamieww@outlook.com>
2018-05-08 23:04:28 +0800

    added all static script files on 2018-05-08

--- Go/hello.go
+++ Go/hello.go
@@ -1,12 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"math"
-	"time"
-)
-
+/*
 func main() {
 	fmt.Println(math.Pi)
 	fmt.Printf("The time is ", time.Now())
+	fmt.Println()
+
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
+}*/
+
+func swap(x, y string) (string, string) {
+	return y, x
 }
